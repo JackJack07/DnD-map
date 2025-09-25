79fcdc895df150eadd5d8e9eec55122e69903259
JackJack07 <fisanmarco@gmail.com>
2025-09-25 22:28:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const grid = document.querySelector('.grid');
+const rows = 10;
+const cols = 10;
+
+for (let i = 0; i < rows * cols; i++) {
+  const tile = document.createElement('div');
+  tile.classList.add('tile');
+  grid.appendChild(tile);
+}
 
